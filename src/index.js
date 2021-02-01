10aa8fb1774de968d0ff6e3699a4687601e1841f
TarasKlochko <60792921+TarasKlochko@users.noreply.github.com>
2021-02-01 14:30:42 +0200

    feat: implement 3 functions: max, min, and avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
+exports.min = function min(array) {
+    let min = 0;
+    if (array && array.length) {
+        array.forEach((element) => (element < min ? (min = element) : min));
+    }
+    return min;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    let max = 0;
+    if (array && array.length) {
+        array.forEach((element) => (element > max ? (max = element) : max));
+    }
+    return max;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    return array && array.length
+        ? array.reduce((sum, number) => sum + number, 0) / array.length
+        : 0;
+};
